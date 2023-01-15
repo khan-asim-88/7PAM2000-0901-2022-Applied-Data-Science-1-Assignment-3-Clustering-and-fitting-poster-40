0a40ce3fc3d75b471cdf375aefbe46404d9ae287
Asim Khan <asimlalaa@gmail.com>
2023-01-15 14:10:13 +0000

    Update Asim_ADS1_Assignment_3_Clustering_and_ftting_Code.py

--- Asim_ADS1_Assignment_3_Clustering_and_ftting_Code.py
+++ Asim_ADS1_Assignment_3_Clustering_and_ftting_Code.py
@@ -92,3 +92,47 @@ y_pred = kmean.predict(X)
 df_new["cluster"] = y_pred
 df_new["Country Name"] = df_new["Country Name"].map({v: k for k, v in count_mapper.items()})
 df_new["AVG_CO2"] = df_new.iloc[:, 1:-1].mean(axis=1)
+
+# PLoting clusters vs Average CO2
+df_new.groupby("cluster").aggregate(["mean"])["AVG_CO2"].plot(kind="bar", figsize=(8, 5))
+plt.title("Cluster VS Average CO2")
+plt.ylabel("Average CO2")
+plt.show()
+
+# Finding cluster in which pakistan lies
+pk_cluster = df_new[df_new["Country Name"] == "Pakistan"].cluster
+print(f"Cluster of Pakistan Country is {pk_cluster.values[0]}")
+
+# Top 5 Countries in Cluster where average co2 is highest
+temp = df_new[df_new.cluster == 0][["Country Name", "AVG_CO2"]].sort_values("AVG_CO2", ascending=False).iloc[:20, :]
+plt.figure(figsize=(14, 6))
+sns.barplot(temp["Country Name"], temp.AVG_CO2)
+plt.title("Top 5 Countries in Cluster where average co2 is highest")
+plt.xticks(rotation=65)
+plt.show()
+
+# Top 5 Countries in Cluster where average co2 is highest
+plt.figure(figsize=(16, 6))
+plt.subplot(1,2,1)
+temp = df_new[df_new.cluster == 1][["Country Name", "AVG_CO2"]].sort_values("AVG_CO2", ascending=False).iloc[5:, :]
+sns.barplot(temp["Country Name"], temp.AVG_CO2)
+plt.title("Top 5 Countries in Cluster where average co2 is highest")
+plt.xticks(rotation=65)
+
+
+# Top 5 Countries in Cluster where average co2 is medium
+plt.subplot(1,2,2)
+temp = df_new[df_new.cluster == 2][["Country Name", "AVG_CO2"]].sort_values("AVG_CO2", ascending=False).iloc[5:, :]
+sns.barplot(temp["Country Name"], temp.AVG_CO2)
+plt.title("Top 6 Countries in Cluster where average co2 is medium")
+plt.xticks(rotation=65)
+plt.show()
+
+# Showing COuntries with respect to Cluster and AVG_CO2
+plt.figure(figsize=(14, 6))
+dots = sns.scatterplot(df_new.X2, df_new.AVG_CO2, hue=y_pred)
+# Add text labels to each dot
+for i, dot in zip(df_new["Country Name"].to_list(), dots.collections[0].get_offsets()):
+    plt.annotate(f"{i}", (dot.data[0] + 0.05, dot.data[1] + 0.05))
+plt.title("Showing COuntries with respect to Cluster and AVG_CO2")
+plt.show()
